c90278ba09178526a3b39f293abbce9cd9b32f72
The Otterlord <reuben.tier@icloud.com>
2021-01-02 14:36:12 +0000

    Upate copyright + add settings class

--- js/settings.js
+++ js/settings.js
@@ -1,3 +1,42 @@
+class Settings {
+  constructor() {
+    this.data = JSON.parse(localStorage.getItem("settings")) ?? {};
+    this.data = this.update();
+  }
+
+  /**
+   * Update settings to the latest layout.
+   * Not used to update based on UI input
+   * @param {object} old Previous settings
+   */
+  update(old) {
+    var new_settings = {};
+    new_settings.keybindings = {...{
+      help: {key: "f1", ctrl: true},
+      open_deck: {key: "o", ctrl: true},
+      save: {key: "s", ctrl: true},
+      save_as: {key: "s", ctrl: true, shift: true},
+      settings: {key: ",", ctrl: true}
+    }, ...old.keybindings ?? {}};
+    new_settings.author = old.author ?? "";
+    this.save();
+    return new_settings;
+  }
+
+  get(key) {
+    return this.data[key];
+  }
+
+  set(key, value) {
+    this.data[key] = value;
+    this.save();
+  }
+
+  save() {
+    localStorage.setItem("settings", JSON.stringify(new_settings));
+  }
+}
+
 function open_settings() {
   document.querySelector(".fade-bg").classList.remove("hide");
   document.querySelector(".settings").classList.add("show");
